2c5ec6d1ef49276f3602cb04f680a0f83da09bd4
danielgalarza-collab <daniel.galarza@busscollab.com>
2025-10-30 23:44:07 +0100

    Lesson-2

---
+++ lesson-2/Script.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void mostrarBinario(int num) {
+    for (int i = 7; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        printf("%d", bit);
+    }
+}
+
+int main() {
+    printf("DECIMAL\tBINARIO\t\tHEXADECIMAL\n");
+    printf("-------------------------------------\n");
+
+    for (int i = 0; i <= 255; i++) {
+        printf("%3d\t", i);
+        mostrarBinario(i);
+        printf("\t0x%02X\n", i);
+    }
+
+    printf("\nPresiona Enter para salir...");
+    getchar();
+    return 0;
+}
+#include <stdio.h>
+
+void mostrarBinario(int num) {
+    for (int i = 7; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        printf("%d", bit);
+    }
+}
+
+int main() {
+    printf("DECIMAL\tBINARIO\t\tHEXADECIMAL\n");
+    printf("-------------------------------------\n");
+
+    for (int i = 0; i <= 255; i++) {
+        printf("%3d\t", i);
+        mostrarBinario(i);
+        printf("\t0x%02X\n", i);
+    }
+
+    printf("\nPresiona Enter para salir...");
+    getchar();
+    return 0;
+}
+l
